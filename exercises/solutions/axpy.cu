24afa6e41b6d8447083f944127a4526c4f9e3b9c
ssande7 <1731652+ssande7@users.noreply.github.com>
2023-03-12 14:23:58 +1000

    Switch to cuda_helpers.cuh

--- exercises/solutions/axpy.cu
+++ exercises/solutions/axpy.cu
@@ -1,27 +1,5 @@
 #include <stdio.h>
-
-/*
-  Helper functions for error checking
-*/
-#define cuda_check_impl(cmd, abort) {                  \
-  cudaError_t status = (cmd);                          \
-  if (status != cudaSuccess) {                         \
-    fprintf(stderr, "CUDA Error: %s (%s:%d)\n",        \
-      cudaGetErrorString(status), __FILE__, __LINE__); \
-    if (abort) exit(status);                           \
-  }                                                    \
-}
-#define cuda_check(cmd) cuda_check_impl((cmd), 1)
-#define cuda_check_noabort(cmd) cuda_check_impl((cmd), 0)
-
-
-/*
-  Helper functions for calculating grid size
-*/
-#define NBLOCKS(N, BLOCK_SIZE) (((N) + (BLOCK_SIZE) - 1)/(BLOCK_SIZE))
-#define DEF_BLOCK 256
-#define DEF_GRID(N) NBLOCKS((N), DEF_BLOCK)
-
+#include "../cuda_helpers.cuh"
 
 /*
  TODO: Add your axpy function here
